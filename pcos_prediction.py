ccb8ca6f3bb319e9ff523abb25da83a58ac23c4e
Sammi <samanthamary391@gmail.com>
2022-01-10 12:38:26 -0500

    Break into functions. Code clean up.

--- pcos_prediction.py
+++ pcos_prediction.py
@@ -4,40 +4,46 @@ import sklearn
 from sklearn.linear_model import LogisticRegression
 from sklearn.utils import shuffle
 
-df = pd.DataFrame(pd.read_csv('PCOS_data_without_infertility.csv'))
-df = df.drop(columns=[
-'Unnamed: 44',
-'Sl. No',
-'Patient File No.',
-'BMI',
-'FSH/LH',
-'Waist:Hip Ratio'
-])
-df = df.drop(305) #invalid data
-df = df.dropna()
+PCOS_DATA = 'PCOS_data_without_infertility.csv'
 
-predict = 'PCOS (Y/N)'
+def load_pcos_data():
+    df = pd.DataFrame(pd.read_csv(PCOS_DATA))
+    df = df.drop(columns=[
+        'Unnamed: 44',
+        'Sl. No',
+        'Patient File No.',
+        'BMI',
+        'FSH/LH',
+        'Waist:Hip Ratio'
+        ])
+    df = df.drop(305) #invalid data
+    df = df.dropna()
 
-X = np.array(df.drop(columns=predict))
-y = np.array(df[predict])
+    return df
 
-# splitting up 5% of the data into testing data and 95%  into training data
-(X_train, X_test, y_train, y_test) = sklearn.model_selection.train_test_split(
-    X,
-    y,
-    test_size=0.05
-)
+def main():
+    pcos_data = load_pcos_data()
 
-logistic = LogisticRegression()
-logistic.fit(X_train, y_train)
-accuracy = logistic.score(X_test, y_test)
+    prediction_column = 'PCOS (Y/N)'
 
-predictions = logistic.predict(X_test)
-print('Prediction  Answer')
-for x in range(len(predictions)):
-    print(predictions[x], '        ' , y_test[x])
+    X = np.array(pcos_data.drop(columns=prediction_column))
+    y = np.array(pcos_data[prediction_column])
 
+    # splitting up 5% of the data into testing data and 95%  into training data
+    (X_train, X_test, y_train, y_test) = sklearn.model_selection.train_test_split(
+        X, y, test_size=0.05
+    )
 
-print('Accuracy: ', accuracy)
+    logistic = LogisticRegression()
+    logistic.fit(X_train, y_train)
+    accuracy = logistic.score(X_test, y_test)
 
-#print(df)
+    predictions = logistic.predict(X_test)
+    print('Prediction  Answer')
+    for i, predictions in enumerate(predictions):
+        print(f'{predictions}         {y_test[i]}')
+
+    print('Accuracy: ', accuracy)
+
+if __name__ == '__main__':
+    main()
