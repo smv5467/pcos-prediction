fb00240e078c9800d7d0033bc4d33bae5568276a
Sammi <samanthamary391@gmail.com>
2022-01-09 23:22:17 -0500

    Switch to logistic regression

--- pcos_prediction.py
+++ pcos_prediction.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 import sklearn
-from sklearn import linear_model
+from sklearn.linear_model import LogisticRegression
 from sklearn.utils import shuffle
 
 df = pd.DataFrame(pd.read_csv('PCOS_data_without_infertility.csv'))
@@ -21,22 +21,23 @@ predict = 'PCOS (Y/N)'
 X = np.array(df.drop(columns=predict))
 y = np.array(df[predict])
 
-# splitting up 10% of the data into testing data and 90%  into training data
+# splitting up 5% of the data into testing data and 95%  into training data
 (X_train, X_test, y_train, y_test) = sklearn.model_selection.train_test_split(
     X,
     y,
-    test_size=0.1
+    test_size=0.05
 )
 
-linear = linear_model.LinearRegression()
-linear.fit(X_train, y_train)
-accuracy = linear.score(X_test, y_test)
+logistic = LogisticRegression()
+logistic.fit(X_train, y_train)
+accuracy = logistic.score(X_test, y_test)
 
-predictions = np.around(np.maximum(linear.predict(X_test), 0))
+predictions = logistic.predict(X_test)
+print('Prediction  Answer')
 for x in range(len(predictions)):
-    print(predictions[x], X_test[x], y_test[x])
+    print(predictions[x], '        ' , y_test[x])
 
 
-print(accuracy)
+print('Accuracy: ', accuracy)
 
 #print(df)
