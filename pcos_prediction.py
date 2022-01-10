8173c02a4eae335748a63228bc8e8848ac71a20d
Sammi <samanthamary391@gmail.com>
2022-01-10 18:57:25 -0500

    Implement classification report

--- pcos_prediction.py
+++ pcos_prediction.py
@@ -3,47 +3,50 @@ import numpy as np
 import sklearn
 from sklearn.linear_model import LogisticRegression
 from sklearn.utils import shuffle
+from sklearn.metrics import classification_report
 
-PCOS_DATA = 'PCOS_data_without_infertility.csv'
+PCOS_DATA_FILENAME = 'PCOS_data_without_infertility.csv'
+PREDICTION_COLUMN = 'PCOS (Y/N)'
+TEST_SIZE = 0.05
+UNUSED_COLUMNS = [
+    'Unnamed: 44',
+    'Sl. No',
+    'Patient File No.',
+    'BMI',
+    'FSH/LH',
+    'Waist:Hip Ratio'
+]
 
 def load_pcos_data():
-    df = pd.DataFrame(pd.read_csv(PCOS_DATA))
-    df = df.drop(columns=[
-        'Unnamed: 44',
-        'Sl. No',
-        'Patient File No.',
-        'BMI',
-        'FSH/LH',
-        'Waist:Hip Ratio'
-        ])
+    df = pd.DataFrame(pd.read_csv(PCOS_DATA_FILENAME))
+    df = df.drop(columns=UNUSED_COLUMNS)
     df = df.drop(305) #invalid data
     df = df.dropna()
 
     return df
 
+
 def main():
     pcos_data = load_pcos_data()
 
-    prediction_column = 'PCOS (Y/N)'
-
-    X = np.array(pcos_data.drop(columns=prediction_column))
-    y = np.array(pcos_data[prediction_column])
+    X = np.array(pcos_data.drop(columns=PREDICTION_COLUMN))
+    y = np.array(pcos_data[PREDICTION_COLUMN])
 
     # splitting up 5% of the data into testing data and 95%  into training data
     (X_train, X_test, y_train, y_test) = sklearn.model_selection.train_test_split(
-        X, y, test_size=0.05
+        X, y, test_size=TEST_SIZE
     )
 
     logistic = LogisticRegression()
     logistic.fit(X_train, y_train)
-    accuracy = logistic.score(X_test, y_test)
 
     predictions = logistic.predict(X_test)
-    print('Prediction  Answer')
-    for i, predictions in enumerate(predictions):
-        print(f'{predictions}         {y_test[i]}')
+    print(classification_report(
+        y_test,
+        predictions,
+        target_names=['Negative', 'Positive']
+    ))
 
-    print('Accuracy: ', accuracy)
 
 if __name__ == '__main__':
     main()
